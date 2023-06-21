697a0dad24aa8b49c08c24feadb35ed4b283a63e
FelipeLEopoldino <felipeleopoldino01@icloud.com>
2023-06-21 10:05:30 -0300

    update img

--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
-import { View } from 'react-native'
+import { Image, View } from 'react-native'
 import { styles } from './styles'
 import { Loading } from './src/screens/loading'
 import { Login } from './src/screens/login'
 import { Home } from './src/screens/home'
 import { Login2 } from './src/screens/login2'
-
 export default function App() {
    return (
       <View style={styles.container}>
